c65db10b9151364a2ce93c46f8dd0a4a899b6848
Arran <arranstrange@googlemail.com>
2025-04-09 12:45:30 +0100

    test: selectedDrinksModal tests added

--- src/components/Drinks/SelectedDrinkModal/selectedDrinkModal.test.tsx
+++ src/components/Drinks/SelectedDrinkModal/selectedDrinkModal.test.tsx
@@ -1,37 +1,46 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import { Drink } from "../../types/types";
+import SelectedDrinkModal from "./selectedDrinkModal";
 
-const testData: Drink[] = [
-  {
-    _id: "1",
-    drinkName: "Test Drink 1",
-    DrinkThumb: "testthumb1.png",
-    shortDescription: "Test description 1",
-    Category: "test",
-    Glass: "highball",
-    Ice: "crushed",
-    Ingredient1: "vodka",
-    Ingredient2: "",
-    Ingredient3: "",
-    Ingredient4: "",
-    Ingredient5: "",
-    Ingredient6: "",
-    Instructions: "",
-  },
-  {
-    _id: "2",
-    drinkName: "Test Drink 2",
-    DrinkThumb: "testthumb2.png",
-    shortDescription: "Test description 2",
-    Category: "test",
-    Glass: "martini",
-    Ice: "cubed",
-    Ingredient1: "gin",
-    Ingredient2: "",
-    Ingredient3: "",
-    Ingredient4: "",
-    Ingredient5: "",
-    Ingredient6: "",
-    Instructions: "",
-  },
-];
+const testDrinkData: Drink = {
+  _id: "1",
+  drinkName: "Test Drink",
+  DrinkThumb: "testthumbnail.png",
+  shortDescription: "Test Description",
+  Category: "Test Category",
+  Glass: "Test Glass",
+  Ice: "Test Ice",
+  Ingredient1: "Test Ingredient 1",
+  Ingredient2: "Test Ingredient 2",
+  Ingredient3: "",
+  Ingredient4: "",
+  Ingredient5: "",
+  Ingredient6: "",
+  Measure1: 10,
+  Measure2: 20,
+  Measure3: undefined,
+  Measure4: undefined,
+  Measure5: undefined,
+  Measure6: undefined,
+  Instructions: "Test instructions",
+};
+
+test("ensure the modal renders with the correct details", () => {
+  const mockOnClose = jest.fn();
+  const mockOnEdit = jest.fn();
+  render(
+    <SelectedDrinkModal
+      drink={testDrinkData}
+      onClose={mockOnClose}
+      onEdit={mockOnEdit}
+      isGuest={false}
+    />
+  );
+  const modalContainer = screen.getByTestId("selected-drink-modal");
+  expect(modalContainer).toBeInTheDocument();
+
+  expect(screen.getByText(testDrinkData.drinkName)).toBeInTheDocument();
+  expect(screen.getByText(testDrinkData.Category)).toBeInTheDocument();
+  expect(screen.getByText(testDrinkData.Instructions)).toBeInTheDocument();
+  expect(screen.getByText(testDrinkData.Ingredient1!)).toBeInTheDocument();
+});
